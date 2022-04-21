32b9115eda4a3403bdbce45601ea167b59bc9167
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-04-21 17:39:19 +0900

    Add keyboard zone

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 
 int main(int argc, char**argv) {
   const int SCORE_LENGTH = 5;
+  const int KEYBOARD_LENGTH = 10;
+
   std::string raw_score[SCORE_LENGTH];
   struct winsize w;
   ioctl(0, TIOCGWINSZ, &w);
@@ -19,14 +21,22 @@ int main(int argc, char**argv) {
   // Load
   for(int l = 0; l < SCORE_LENGTH; l++){
     for (int i = 0; i < w.ws_col; i++) {
-      if(i == 5) raw_score[l] += '1';
+      if(i == 5 || i == 6 || i == 20 || i == 21) raw_score[l] += '1';
       else raw_score[l] += '9';
     }
   }
-
+  // std::cout << "\033[100m";
+  // for(int line = 0; line < w.ws_row - KEYBOARD_LENGTH; line++) std::cout << std::string(w.ws_col, ' ');
+  // printf("\033[%d;%dH", w.ws_row - KEYBOARD_LENGTH, 0);
+  std::cout << "\033[" << w.ws_row - KEYBOARD_LENGTH + 1 << ";" << 0 << "H";
+  std::cout << "\033[47m";
+  std::cout << std::string(w.ws_col * KEYBOARD_LENGTH, ' ');
+  std::cout << "\033[49m";
+  std::cout << std::flush;
+  usleep(1000000);
   // Show
-  for(int current_loc = - w.ws_row; current_loc < SCORE_LENGTH; current_loc++){
-    for(int line = 0; line < w.ws_row; line++){
+  for(int current_loc = - w.ws_row; current_loc < SCORE_LENGTH - KEYBOARD_LENGTH; current_loc++){
+    for(int line = 0; line < w.ws_row - KEYBOARD_LENGTH; line++){
       const int index = w.ws_row - line + current_loc;
       std::string out_text;
       if(0 <= index && index < SCORE_LENGTH) out_text = raw_score[index];
