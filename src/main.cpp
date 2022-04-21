5d5830218334f90b0e61359f0159254b4b66f768
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-04-21 16:39:51 +0900

    Write to console on demand

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <array>
+
 int main(int argc, char**argv) {
   const int SCORE_LENGTH = 5;
   std::string raw_score[SCORE_LENGTH];
@@ -13,22 +14,36 @@ int main(int argc, char**argv) {
   printf("\033[2J");
   printf("\033[%d;%dH" ,0, 0);
 
+  std::string current_display[w.ws_row];
+
+  // Load
   for(int l = 0; l < SCORE_LENGTH; l++){
     for (int i = 0; i < w.ws_col; i++) {
-      if(i == 5) raw_score[l] += "\033[41m";
-      raw_score[l] += " ";
-      if(i - 1 == 5) raw_score[l] += "\033[49m";
+      if(i == 5) raw_score[l] += '1';
+      else raw_score[l] += '9';
     }
   }
+
+  // Show
   for(int current_loc = - w.ws_row; current_loc < SCORE_LENGTH; current_loc++){
-    printf("\033[2J");
-    printf("\033[%d;%dH" ,0, 0);
     for(int line = 0; line < w.ws_row; line++){
       const int index = w.ws_row - line + current_loc;
-      if(0 <= index && index < SCORE_LENGTH) std::cout << raw_score[index] << std::endl;
-      else std::cout << std::string(w.ws_col, ' ') << std::endl;
+      std::string out_text;
+      if(0 <= index && index < SCORE_LENGTH) out_text = raw_score[index];
+      else out_text = std::string(w.ws_col, '9');
+      for(int letter = 0; letter < w.ws_col; letter++){
+        if(current_display[line][letter] != out_text[letter]){
+          std::cout << "\033[" << line + 1 << ";" << letter + 1 << "H"; // << std::flush;
+          std::cout << "\033[4" << out_text[letter] << "m"; // << std::flush;
+          std::cout << " "; // << std::flush;
+          std::cout << "\033[49m";
+        }
+      }
+      current_display[line] = out_text;
     }
+    std::cout << std::flush;
     usleep(100000);
   }
+
   return 0;
 }
